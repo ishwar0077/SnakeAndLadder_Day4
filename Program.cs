d163e8356356dbc63fc25572e9bcbcca02b0c7cd
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-24 18:46:28 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UseCase1
+    {
+        public static void rolltheDie()
+        {
+            //the player is at zero position
+            Console.WriteLine("Welcome to Snake And lagdder Program");
+        }
+    }
+}
